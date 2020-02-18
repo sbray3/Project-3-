63cd604eb6958ee57b91573bb580d8d2708eb667
sbray3 <sarahsarahbrady@gmail.com>
2020-02-18 17:58:25 -0600

    Update days counter

--- Python_Code.py
+++ Python_Code.py
@@ -7,3 +7,14 @@ print("Number of lines: ",count)
 
 
 f.close()
+
+import datetime
+a = open("http_access_log.txt", "r")
+b = datetime.date(1994,10,24)
+count = 0
+
+for line in a:
+     count ("b" +=1)
+    print("Days: ", count)
+
+
