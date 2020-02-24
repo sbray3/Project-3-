46f1057eb2c32968919ad051af4b091c32e05ad8
sbray3 <sarahsarahbrady@gmail.com>
2020-02-23 18:18:53 -0600

    Updated #2

--- Python_Code.py
+++ Python_Code.py
@@ -1,20 +1,41 @@
+import re
+# This counts the # of lines
 f = open("http_access_log.txt", "r")
 count = 0
-
 for line in f:
      count +=1
 print("Number of lines: ",count)
-
-
 f.close()
 
-import datetime
-a = open("http_access_log.txt", "r")
-b = datetime.date(1994,10,24)
-count = 0
+# Get the count of stuff
 
-for line in a:
-     count ("b" +=1)
-    print("Days: ", count)
+day = 0
+month = ""
+year = 0
+day_count = 0
+month_count = 0
+year_count = 0
 
+with open("http_access_log.txt","r") as f:
+     for line in f:
+          # Look for a match in the line
+          date = re.search(r'\[(\d+)/(.+)/(\d+).+\]', line)
+          # check if there was as match in the line
+          if(date != None):
+               d = date.group(1)
+               m = date.group(2)
+               y = date.group(3)
+               if(d != day):
+                    day = d
+                    day_count += 1
+               if(m != month):
+                    month = m
+                    month_count += 1
+               if(y != year):
+                    year = y
+                    year_count += 1
+
+print("The # of different days: ", day_count)
+print("The # of different months: ", month_count)
+print("The # of different years: ", year_count)
 
