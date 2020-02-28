2afe545aa04a233634cacadbbaf9b86fa6509bc8
sbray3 <sarahsarahbrady@gmail.com>
2020-02-27 20:51:49 -0600

    Updated #5 and #6, added answer file

--- Python_Code.py
+++ Python_Code.py
@@ -1,5 +1,7 @@
 import re
 
+print("This takes around 5 minutes to run on my machine, it is working...")
+
 # Get the count of stuff
 count = 0
 day = 0
@@ -11,16 +13,20 @@ year_count = 0
 error_count = 0
 redir_count = 0
 
+array = []
+
 with open("http_access_log.txt","r") as f:
      for line in f:
           count +=1
           # Look for a match in the line
-          date = re.search(r'\[(\d+)/(.+)/(\d+).+\]\s(\".+\")\s(\d+)', line)
+          date = re.search(r'\[(\d+)/(.+)/(\d+).+\]\s\".+\s(.+\..+)\s.+\"\s(\d+)', line)
           # check if there was as match in the line
           if(date != None):
                d = date.group(1)
                m = date.group(2)
                y = date.group(3)
+               f = date.group(4)
+               # print(f)
                s = date.group(5)
                if(d != day):
                     day = d
@@ -31,10 +37,26 @@ with open("http_access_log.txt","r") as f:
                if(y != year):
                     year = y
                     year_count += 1
-               if( s[0]=="4"):
+               if(s[0]=="4"):
                     error_count += 1
-               if( s[0]=="3"):
-                    redir_count +=1
+               if(s[0]=="3"):
+                    redir_count += 1
+
+               # [      0     ,      1     ]
+               #  [  0  ,  1 ] [   0 ,  1 ]
+               # [["file",cnt],["file",cnt]]
+               found = False
+               for index in range(len(array)):
+                    if(array[index][0] == f):
+                         array[index][1] += 1
+                         found = True
+                         break
+               if(not found):
+                    array.append([f,1])
+                    
+array.sort(key=lambda x: x[1])                   
+
+
 
                          
 
@@ -45,3 +67,5 @@ print("The # of different years: ", year_count)
 print("The # of error codes: ", error_count)
 print("The # of redirected codes: ", redir_count)
 
+print("Most searched: ", str(array[-1]))
+print("Least searched: ", str(array[0]))
